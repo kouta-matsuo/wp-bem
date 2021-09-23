a6c13ab9ddf176976ebe19800b3d920b45049c49
Ubuntu <ubuntu@ip-172-31-37-124.ap-northeast-1.compute.internal>
2021-09-23 01:13:15 +0000

    bulk-child-bem-before

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php if ( is_active_sidebar( 'bulk-footer-area' ) ) { ?>  				
+	<div id="content-footer-section" class="row clearfix">
+		<div class="container">
+			<?php dynamic_sidebar( 'bulk-footer-area' ) ?>
+		</div>	
+	</div>		
+<?php } ?> 
+</div>
+<footer id="colophon" class="footer-credits container-fluid row">
+	<div class="container text-center">
+    <small >©️Kouta.2021</small>
+	</div>	
+</footer>
+<!-- end main container -->
+</div>
+<?php wp_footer(); ?>
+
+</body>
+</html>
